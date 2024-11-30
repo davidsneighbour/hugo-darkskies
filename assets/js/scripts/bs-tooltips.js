2d338de8ddb154e4a8ddf41e83930ac78f650223
Patrick Kollitsch <patrick@davids-neighbour.com>
2024-11-30 20:52:38 +0700

    theme(fix): add dropdowns to top navigation
    Signed-off-by: Patrick Kollitsch <patrick@davids-neighbour.com>

--- assets/js/scripts/bs-tooltips.js
+++ assets/js/scripts/bs-tooltips.js
@@ -1,8 +1,11 @@
 // import { createPopper } from '@popperjs/core';
-import { Tooltip } from 'bootstrap';
+import { Tooltip, Dropdown } from 'bootstrap';
 
 // Bootstrap Tooltips
-var tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-var tooltipList = Array.from(tooltipTriggerList).map(function (tooltipTriggerEl) {
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = Array.from(tooltipTriggerList).map(function (tooltipTriggerEl) {
   return new Tooltip(tooltipTriggerEl);
 });
+
+const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
+const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new Dropdown(dropdownToggleEl))
